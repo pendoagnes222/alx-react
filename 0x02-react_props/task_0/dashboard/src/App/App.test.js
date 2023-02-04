38f27ee2a71ccdc686be6a88831272b4b0d0b8a5
PENDO AGNES <99383447+pendoagnes222@users.noreply.github.com>
2023-02-04 23:13:04 +0300

    task

---
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -0,0 +1,25 @@
+import App from './App';
+import { shallow, render } from 'enzyme';
+import React from 'react'
+
+describe('App', () => {
+    test('App should not rash', () => {
+        render(<App />)
+    });
+
+    // test("App renders a div with the class: App-header", () => {
+    //     const wrapper = shallow(<App />);
+    //     expect(wrapper.find('.App-header').length).toBe(1)
+    // });
+
+    test("App renders a div with the class: App-body", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('.App-body').length).toBe(1);
+    });
+
+    test("App renders a div with the class: App-footer", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('.App-footer').length).toBe(1);
+    });
+ 
+});
